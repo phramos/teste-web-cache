2cd259f0f14211c448877aa915bb56c888cb6993
Pedro Souza <pedro@vencis.com.br>
2017-03-11 20:24:48 -0300

    added default port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 8000;
 
 app.get('/', function (req, res) {
     res.setHeader('Cache-Control', 'public, max-age=30');
